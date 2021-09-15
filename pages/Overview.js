db7260cb5023aa69e3c21e8e6fc7a892b9da3cc3
rubakanaan <rubajkanaan@gmail.com>
2021-09-15 15:45:02 +0300

    class38 done

--- pages/Overview.js
+++ pages/Overview.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Overview(props) {
     return (
-        <div  className="p-4 mt-8 text-3xl rounded-lg 	bg-green-500 text-black-50">       
+        <div  className="p-4  text-3xl  	bg-green-500 text-black-50">       
         <Link href="/"><a>Return to Main page</a></Link>
 
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header'
 import CreateForm from '../components/CreateForm'
 import ReportTable from '../components/ReportTable'
@@ -8,15 +8,28 @@ import { hours } from "../data";
 export default function Home() {
 
   const [reports, setCookieData] = useState([]);
+  const [totals, setTotals] = useState()
+  const [hourlyTotals, SetHourlyTotal] = useState()
+
+
   function onCreate(event) {
     event.preventDefault();
+    let numberOfCookie = []
+    let total = 0
+    for (let i = 0; i < 14; i++) {
+      let customer = getRandomInt(parseInt(event.target.minimumCustomerperHour.value), parseInt(event.target.maximumCustomerperHour.value));
+      let cookie = Math.ceil(customer * parseInt(event.target.averageCookiespersale.value));
+      numberOfCookie.push(cookie);
+      total = total + cookie;
+    }
 
     const reports = {
       location: event.target.location.value,
       minCustomerPerHour: event.target.minimumCustomerperHour.value,
       maxCustomerPerHour: event.target.maximumCustomerperHour.value,
       avgCookiePerSale: event.target.averageCookiespersale.value,
-      hourly_sales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
+      hourly_sales: numberOfCookie,
+      total: total
     }
 
     setCookieData(data => [...data, reports])
@@ -24,6 +37,46 @@ export default function Home() {
 
   }
 
+
+  const sumtotals = () => {
+    let sumtotal = 0
+    reports.map(report => {
+      report.hourly_sales.map(sale => {
+        sumtotal += sale
+      })
+    })
+    setTotals(sumtotal)
+  }
+
+
+  useEffect(() => {
+    sumtotals()
+  }, [reports])
+
+
+  function hourlyTotal() {
+    let hourly_totals = [];
+    for (let i = 0; i < 14; i++) {
+      let hour_totals = 0;
+      for (let j = 0; j < reports.length; j++) {
+        hour_totals += reports[j].hourly_sales[i];
+      }
+      hourly_totals.push(hour_totals);
+    }
+    SetHourlyTotal(hourly_totals)
+  }
+
+  useEffect(() => {
+    hourlyTotal()
+  }, [reports])
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  
 
   return (
     <div className="bg-green-50 ">
@@ -35,9 +88,12 @@ export default function Home() {
       <main className="">
         <CreateForm onCreate={onCreate}
         />
+
         <ReportTable
           hours={hours}
           reports={reports}
+          totals={totals}
+          hourlyTotals={hourlyTotals}
         />
         <Footer
           reports={reports}
