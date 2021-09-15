c4f355c577271cc14c8d4d951ba4709e506193c7
rubakanaan <rubajkanaan@gmail.com>
2021-09-15 20:29:58 +0300

    login form

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -1,24 +1,24 @@
 export default function CreateForm(props) {
     return (
-        <form className="m-9 p-5 mx-auto w-3/4 my-7  rounded-lg  bg-green-300" onSubmit={props.onCreate}>
-            <legend className="text-center my-3 text-xl font-semibold">Create Cookie Stand</legend>
+        <form className="w-3/4 p-5 mx-auto bg-green-300 rounded-lg m-9 my-7" onSubmit={props.onCreate}>
+            <legend className="my-3 text-xl font-semibold text-center">Create Cookie Stand</legend>
             <label className="" >Location </label>
-            <input name='location' className='w-11/12 ' />
+            <input name='location' className='w-11/12 ' required />
             <div class="grid grid-cols-4 my-5 text-center gap-2 ">
                 <div class=" rounded-lg bg-green-100">
                     <label >Minimum Customer per Hour </label>
-                    <input name="minimumCustomerperHour" className="" />
+                    <input name="minimumCustomerperHour" className="" required />
                 </div>
                 <div class=" rounded-lg bg-green-100">
                     <label >Maximum Customer per Hour </label>
-                    <input name='maximumCustomerperHour' className="" />
+                    <input name='maximumCustomerperHour' className="" required />
                 </div>
                 <div class="rounded-lg bg-green-100">
                     <label >Average Cookies per sale </label>
-                    <input name='averageCookiespersale' className="" />
+                    <input name='averageCookiespersale' className="" required />
                 </div>
                 <div class=" ">
-                    <button className="w-64 h-20 bg-green-500 text-black-50 rounded-lg">Create</button>
+                    <button className="w-64 h-20 bg-green-500 rounded-lg text-black-50">Create</button>
                 </div>
             </div>
         </form>
